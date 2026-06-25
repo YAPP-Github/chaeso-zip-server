59defeecb14215f4caf1c3f537a2e7326144c867
SinnoLn <ok373737@naver.com>
2026-06-26 00:12:49 +0900

    test: Enforce DTO and entity placement with ArchUnit

--- src/test/java/chaeso/zip/server/architecture/LayerDependencyTest.java
+++ src/test/java/chaeso/zip/server/architecture/LayerDependencyTest.java
@@ -8,6 +8,7 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.library.Architectures;
+import jakarta.persistence.Entity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,4 +55,42 @@ class LayerDependencyTest {
       .that().areAnnotatedWith(Service.class)
       .should().resideInAPackage("..application..")
       .as("@Service 는 application 패키지에만 위치해야 한다");
+
+  @ArchTest
+  static final ArchRule 엔티티는_도메인_계층에만_위치한다 = classes()
+      .that().areAnnotatedWith(Entity.class)
+      .should().resideInAPackage("..domain..")
+      .as("@Entity 는 domain 패키지에만 위치해야 한다");
+
+  @ArchTest
+  static final ArchRule 표현_계층은_엔티티에_직접_의존하지_않는다 = noClasses()
+      .that().resideInAPackage("..presentation..")
+      .should().dependOnClassesThat().areAnnotatedWith(Entity.class)
+      .as("presentation 은 엔티티를 직접 참조하지 말고 application DTO 를 거쳐야 한다");
+
+  @ArchTest
+  static final ArchRule 리포지토리는_도메인_계층에_위치한다 = classes()
+      .that().haveSimpleNameEndingWith("Repository")
+      .should().resideInAPackage("..domain..")
+      .as("*Repository 는 domain 패키지에 위치해야 한다");
+
+  @ArchTest
+  static final ArchRule 요청_DTO는_표현_계층에_위치한다 = classes()
+      .that().haveSimpleNameEndingWith("Request")
+      .and().resideOutsideOfPackage("..common..")
+      .should().resideInAPackage("..presentation..")
+      .as("*Request DTO 는 presentation 패키지에 위치해야 한다");
+
+  @ArchTest
+  static final ArchRule 커맨드_DTO는_애플리케이션_계층에_위치한다 = classes()
+      .that().haveSimpleNameEndingWith("Command")
+      .should().resideInAPackage("..application..")
+      .as("*Command DTO 는 application 패키지에 위치해야 한다");
+
+  @ArchTest
+  static final ArchRule 응답_DTO는_애플리케이션_계층에_위치한다 = classes()
+      .that().haveSimpleNameEndingWith("Response")
+      .and().resideOutsideOfPackage("..common..")
+      .should().resideInAPackage("..application..")
+      .as("*Response DTO 는 application 패키지에 위치해야 한다 (공통 응답 래퍼는 common 에 둔다)");
 }
