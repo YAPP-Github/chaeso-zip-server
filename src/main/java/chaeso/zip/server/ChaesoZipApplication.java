c598f7519092e4d241b4b3a35d49c56cbe69df35
Yelim Lee <lee_yerim@naver.com>
2026-06-25 09:08:27 +0900

    init: Initialize Project

---
+++ src/main/java/chaeso/zip/server/ChaesoZipApplication.java
@@ -0,0 +1,13 @@
+package chaeso.zip.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaesoZipApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChaesoZipApplication.class, args);
+  }
+
+}
---
+++ src/test/java/chaeso/zip/server/ChaesoZipApplicationTests.java
@@ -0,0 +1,13 @@
+package chaeso.zip.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaesoZipApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
