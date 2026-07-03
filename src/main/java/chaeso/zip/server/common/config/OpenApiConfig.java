ca6d02c69e2accb00302e62b9f5b04fd18df7af2
SinnoLn <ok373737@naver.com>
2026-07-04 01:45:17 +0900

    feat(auth): add JWT bearer security scheme to OpenAPI config

--- src/main/java/chaeso/zip/server/common/config/OpenApiConfig.java
+++ src/main/java/chaeso/zip/server/common/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package chaeso.zip.server.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,6 +30,12 @@ public class OpenApiConfig {
             .description("채소.zip 서비스 API 문서")
             .version("v1")
             .contact(new Contact().name("chaeso-zip").email("channelsogae.zip@gmail.com"))
-            .license(new License().name("Apache 2.0")));
+            .license(new License().name("Apache 2.0")))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(new Components().addSecuritySchemes("bearerAuth",
+            new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")));
   }
 }
