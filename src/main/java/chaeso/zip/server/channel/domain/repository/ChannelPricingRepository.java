d510f8b87b19b0b5625adf4f70f791240a8b4a6d
Yelim Lee <lee_yerim@naver.com>
2026-07-20 14:55:06 +0900

    test(channel): verify catalog array/enum mapping against loaded data

--- src/main/java/chaeso/zip/server/channel/domain/repository/ChannelPricingRepository.java
+++ src/main/java/chaeso/zip/server/channel/domain/repository/ChannelPricingRepository.java
@@ -1,8 +1,11 @@
 package chaeso.zip.server.channel.domain.repository;
 
 import chaeso.zip.server.channel.domain.entity.ChannelPricing;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChannelPricingRepository extends JpaRepository<ChannelPricing, UUID> {
+
+  List<ChannelPricing> findByChannelProductId(UUID channelProductId);
 }
