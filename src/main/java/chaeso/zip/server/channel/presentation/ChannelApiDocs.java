2a28808abbf68480c22ff36778cd887f0736f1b1
Lee Jiny <ok373737@naver.com>
2026-07-21 19:07:20 +0900

    feat(openapi): explode Pageable into individual query parameters (#65)

--- src/main/java/chaeso/zip/server/channel/presentation/ChannelApiDocs.java
+++ src/main/java/chaeso/zip/server/channel/presentation/ChannelApiDocs.java
@@ -5,6 +5,7 @@ import chaeso.zip.server.common.response.ApiResponse;
 import chaeso.zip.server.common.response.PageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 
 @Tag(name = "Channel", description = "채널 카탈로그 API")
@@ -13,5 +14,5 @@ public interface ChannelApiDocs {
   @Operation(operationId = "getChannels", summary = "채널 목록 조회",
       description = "전체 채널을 페이지 단위로 조회한다. 인증 없이 접근 가능하며, 미지정 시 이름순 12개.")
   @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "조회 성공")
-  ApiResponse<PageResponse<ChannelListItemResponse>> getChannels(Pageable pageable);
+  ApiResponse<PageResponse<ChannelListItemResponse>> getChannels(@ParameterObject Pageable pageable);
 }
