2fe78627291bcfaff99ea8de1f0a9d3177c46c2e
Yelim Lee <lee_yerim@naver.com>
2026-07-21 13:49:00 +0900

    refactor(channel): move channel Swagger to ChannelApiDocs interface

--- src/main/java/chaeso/zip/server/channel/presentation/ChannelController.java
+++ src/main/java/chaeso/zip/server/channel/presentation/ChannelController.java
@@ -4,8 +4,6 @@ import chaeso.zip.server.channel.application.ChannelService;
 import chaeso.zip.server.channel.application.dto.ChannelListItemResponse;
 import chaeso.zip.server.common.response.ApiResponse;
 import chaeso.zip.server.common.response.PageResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -13,16 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Channel", description = "채널 카탈로그 API")
 @RestController
 @RequestMapping("/api/v1/channels")
 @RequiredArgsConstructor
-public class ChannelController {
+public class ChannelController implements ChannelApiDocs {
 
   private final ChannelService channelService;
 
-  @Operation(operationId = "getChannels", summary = "채널 목록 조회",
-      description = "전체 채널을 페이지 단위로 조회한다. (기본값: 페이지당 12개, 정렬 이름순)")
+  @Override
   @GetMapping
   public ApiResponse<PageResponse<ChannelListItemResponse>> getChannels(
       @PageableDefault(size = 12, sort = "name") Pageable pageable) {
