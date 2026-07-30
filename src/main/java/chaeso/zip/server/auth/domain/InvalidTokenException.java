44b39eaa3abe4ed83dba1633149e6c3d8eb7eb0e
Lee Jiny <ok373737@naver.com>
2026-07-30 16:24:14 +0900

    chore: Auth 예외 계층 정리 및 Swagger 에러 응답 예시 보강 (#99)
    * refactor(auth): make InvalidTokenException extend AuthBusinessException
    
    * docs(auth): add INTERNAL_SERVER_ERROR example to common error response schema

--- src/main/java/chaeso/zip/server/auth/domain/InvalidTokenException.java
+++ src/main/java/chaeso/zip/server/auth/domain/InvalidTokenException.java
@@ -1,11 +1,10 @@
 package chaeso.zip.server.auth.domain;
 
-import chaeso.zip.server.common.exception.BusinessException;
-
 /**
  * 토큰이 유효하지 않을 때 발생하는 도메인 예외.
  */
-public class InvalidTokenException extends BusinessException {
+@SuppressWarnings("java:S110")
+public class InvalidTokenException extends AuthBusinessException {
 
   public InvalidTokenException(String message) {
     super(AuthErrorCode.INVALID_TOKEN, message);
