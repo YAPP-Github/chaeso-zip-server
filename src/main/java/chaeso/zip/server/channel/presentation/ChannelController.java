a5ecd6570823814eae1fa346ede53c46640ecc41
Yelim Lee <lee_yerim@naver.com>
2026-07-31 17:26:27 +0900

    refactor(channel): move query parameter conversion out of the controller

--- src/main/java/chaeso/zip/server/channel/presentation/ChannelController.java
+++ src/main/java/chaeso/zip/server/channel/presentation/ChannelController.java
@@ -3,17 +3,17 @@ package chaeso.zip.server.channel.presentation;
 import chaeso.zip.server.channel.application.ChannelService;
 import chaeso.zip.server.channel.application.dto.ChannelDetailResponse;
 import chaeso.zip.server.channel.application.dto.ChannelListItemResponse;
+import chaeso.zip.server.channel.presentation.dto.ChannelSearchRequest;
 import chaeso.zip.server.common.response.ApiResponse;
 import chaeso.zip.server.common.response.PageResponse;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,9 +26,10 @@ public class ChannelController implements ChannelApiDocs {
   @Override
   @GetMapping
   public ApiResponse<PageResponse<ChannelListItemResponse>> getChannels(
-      @RequestParam(required = false) String name,
-      @PageableDefault(size = 12, sort = "name") @ParameterObject Pageable pageable) {
-    return ApiResponse.success(PageResponse.from(channelService.getChannels(name, pageable)));
+      @ParameterObject ChannelSearchRequest request,
+      @SortDefault(sort = "name") @ParameterObject Sort sort) {
+    return ApiResponse.success(PageResponse.from(
+        channelService.getChannels(request.name(), request.toPageable(sort))));
   }
 
   @Override
